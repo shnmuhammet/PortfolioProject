2a19e509c31e794c6577ed1fdc92edf29f5ac386
Shnmuhammet <shnn.muhammetemin@gmail.com>
2024-10-23 01:11:27 +0300

    .

---
+++ Data Access Layer/Entities/Feature.cs
@@ -0,0 +1,9 @@
+﻿namespace PortfolioProject.Data_Access_Layer.Entities
+{
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
